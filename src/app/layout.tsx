84c7b8474409d927c58e92ec0b00d2e6b792740f
Dastan Mukeev <55836145+thedastan@users.noreply.github.com>
2025-01-29 15:08:53 +0600

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,22 +2,47 @@ import type { Metadata } from "next";
 import "@/styles/globals.scss";
 import LayoutPage from "./provider";
 import { SITE_NAME } from "@/constants/seo/seo.constants";
-import Head from "./Head";
 import { inter } from "@/constants/fonts/fonts";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: SITE_NAME,
   description: "Быстрая услуга портера!",
+  themeColor: "#ffffff",
+  verification: {
+    google: "fQ0IqJz2eTDIDxVnU7cEV8OYPUrHscc9wXzSRQaozgY",
+  },
+  openGraph: {
+    title: SITE_NAME,
+    description: "Быстрая услуга портера!",
+  },
+  robots: "index, follow",
 };
 
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en">
-      <Head />
+      <head>
+        {/* Google Analytics */}
+        <Script
+          strategy="afterInteractive"
+          src="https://www.googletagmanager.com/gtag/js?id=G-FT4V71V5R5"
+        />
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-FT4V71V5R5');
+            `,
+          }}
+        />
+      </head>
       <body className={inter.className}>
         <LayoutPage>{children}</LayoutPage>
       </body>
